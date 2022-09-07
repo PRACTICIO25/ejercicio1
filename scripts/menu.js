bce271d038d0cf36e1b295a65d264a90a8869b48
PRACTICIO25 <111673436+PRACTICIO25@users.noreply.github.com>
2022-09-07 16:39:24 -0500

    Actualización_1.1

---
+++ scripts/menu.js
@@ -0,0 +1,14 @@
+function desplegar(){
+    let ancla = document.getElementsByClassName("etiquetas");
+    for(let i = 0; i < ancla.length; i++){
+        ancla[i].classList.toggle("ocultar");
+    }
+
+    let ancla1 = document.getElementsByClassName("p1");
+    for(let i = 0; i < ancla.length; i++){
+        ancla1[i].classList.toggle("ocultar");
+    }
+    if (typeof foo !== 'undefined') {
+        // Ahora sabemos que foo está definido, ahora podemos continuar.
+      }
+}
